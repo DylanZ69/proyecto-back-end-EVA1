381cca09fdd21829b46f667fbf9e49215d2e6654
DylanZ69 <dcubilloszamora05@gmail.com>
2025-12-13 01:51:46 -0300

    ultimas configuraciones
    se intento volver a usar los roles pero no funcionaron correctamente

--- AdoptaUnaPatitaProyecto/backend_api/api/models.py
+++ AdoptaUnaPatitaProyecto/backend_api/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Refugio(models.Model):
     nombre = models.CharField(max_length=100)
@@ -21,3 +22,10 @@ class Solicitud(models.Model):
 
     # antes era FK -> ahora texto
     mascota_nombre = models.CharField(max_length=100)
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rol = models.CharField(max_length=20, default="usuario")  # admin o usuario
+
+    def __str__(self):
+        return f"{self.user.username} ({self.rol})"
--- AdoptaUnaPatitaProyecto/backend_api/backend_api/settings.py
+++ AdoptaUnaPatitaProyecto/backend_api/backend_api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'drf_spectacular',
-    'api'
+    'api.apps.ApiConfig',
 ]
 
 INSTALLED_APPS += ["rest_framework.authtoken"]
